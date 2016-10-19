6aa3cf98cf5fdfa0ed7613b9351b46295a88d9bc
Vidia <vidia.microad@gmail.com>
2016-10-19 23:22:10 +0700

    add new floating button

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['firebase','ionic', 'starter.controllers', 'starter.services' , 'starter.directives'])
+angular.module('starter', ['firebase','ionic', 'starter.controllers', 'starter.services' , 'starter.directives', 'fabDirective'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
