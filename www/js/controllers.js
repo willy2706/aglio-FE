77d8c23f073228b674cb0ff3dbc855f65dc8d265
Willy Lie <willy.lie@airyrooms.com>
2016-10-27 09:56:35 +0700

    date picker

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,10 +17,41 @@ angular.module('starter.controllers', [])
     }
   })
 
-  .controller('ShareFoodCtrl', function($state, $scope, $firebaseObject, $firebaseArray, $ionicModal, Camera) {
+  .controller('ShareFoodCtrl', function($state, $scope, $firebaseObject, $firebaseArray, $ionicModal, Camera, ionicDatePicker) {
     $scope.gomain = function() {
       $state.go('main')
     }
+    var ipObj1 = {
+      callback: function (val) {  //Mandatory
+        var dateval = new Date(val);
+        var theyear=dateval.getFullYear()
+        var themonth=dateval.getMonth()+1
+        var thetoday=dateval.getDate()
+        $scope.exp_datedisplay = theyear+"/"+themonth+"/"+thetoday
+        $scope.sharefood.exp_date = val
+        console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+      },
+      disabledDates: [            //Optional
+        new Date(2016, 2, 16),
+        new Date(2015, 3, 16),
+        new Date(2015, 4, 16),
+        new Date(2015, 5, 16),
+        new Date('Wednesday, August 12, 2015'),
+        new Date("08-16-2016"),
+        new Date(1439676000000)
+      ],
+      from: new Date(2012, 1, 1), //Optional
+      to: new Date(2016, 10, 30), //Optional
+      inputDate: new Date(),      //Optional
+      mondayFirst: true,          //Optional
+      disableWeekdays: [0],       //Optional
+      closeOnSelect: false,       //Optional
+      templateType: 'popup'       //Optional
+    };
+
+    $scope.openDatePicker = function(){
+      ionicDatePicker.openDatePicker(ipObj1);
+    };
     $scope.showinput = false;
     $scope.photo = function() {
         Camera.getPicture({
